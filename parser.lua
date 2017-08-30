0a04b3ac945989da89c65f95bc9c028c5af184b2
David L. Inman <davidlevere@gmail.com>
2017-08-29 22:03:38 -0400

    assignment parser

--- parser.lua
+++ parser.lua
@@ -47,6 +47,8 @@ function stm()
 
     if false then
 
+    elseif try( "var" ) then 
+        return var_stm()
     elseif try( "while" ) then
         return while_stm()
     elseif try( "foreach" ) then
@@ -61,6 +63,8 @@ function expr()
 
     if false then
 
+    elseif try( "symbol" ) then
+        return { name = "var", value = c.values[1] }
     elseif try( "digits" ) then
         return { name = "digits", value = c.values[1] }
     else
@@ -101,6 +105,18 @@ function foreach_stm()
     return { name = "foreach"; var_name = var_name; seq_expr = seq_expr; body = body }
 end
 
+function var_stm()
+    local c = ct() 
+    is( "symbol" )
+    local var_name = c.values[1]
+    
+    is( "equal" )
 
+    local assign_expr = expr()
+
+    is( "semicolon" ) 
+
+    return { name = "assign"; var_name = var_name; assign_expr = assign_expr }
+end
 
 
